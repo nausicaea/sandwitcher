8fdf0c744c3e514197b16b4fa090465d4116fa53
Eleanore C. Young <email@bugmenot.com>
2016-09-11 14:41:19 +0200

    Created a setup.py file, added verioneer

---
+++ setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# Install dependencies from a "[metadata] setup-requires = ..." section in
+# setup.cfg, then run real-setup.py (or inline setup.py)
+# From https://bitbucket.org/dholth/setup-requires
+
+import os
+import configparser
+import pkg_resources
+import sys
+import subprocess
+import versioneer
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = "Sandwich password generator"
+LONG_DESCRIPTION = ""
+DIST_NAME = "sandwitcher"
+LICENSE = "MIT"
+AUTHOR = "Eleanore C. Young"
+EMAIL = ""
+URL = ""
+DOWNLOAD_URL = ""
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: End Users",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+KEYWORDS = "security, password"
+
+
+def get_requirements():
+    """
+    Get the project's setup-requires requirements from setup.cfg.
+
+    :return:
+    """
+    if not os.path.exists("setup.cfg"):
+        return
+
+    config = configparser.ConfigParser()
+    config.read("setup.cfg", encoding="utf-8")
+    setup_requires = config.get("metadata", "setup-requires")
+    specifiers = [line.strip() for line in setup_requires.splitlines()]
+    for specifier in specifiers:
+        try:
+            pkg_resources.require(specifier)
+        except pkg_resources.DistributionNotFound:
+            yield specifier
+
+
+if __name__ == "__main__":
+    sys.path[0:0] = ["setup-requires"]
+    pkg_resources.working_set.add_entry("setup-requires")
+
+    try:
+        to_install = list(get_requirements())
+        if to_install:
+            subprocess.call([sys.executable, "-m", "pip", "install", "-t", "setup-requires"] + to_install)
+
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        pass
+
+    setup(
+        name=DIST_NAME,
+        author=AUTHOR,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        classifiers=CLASSIFIERS,
+        keywords=KEYWORDS,
+        long_description=LONG_DESCRIPTION,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        platforms="any",
+        install_requires=[
+            "click"
+        ],
+        entry_points={
+            "console_scripts": [
+                "sandwitcher = sandwitcher.main:main"
+            ]
+        },
+        packages=find_packages(where="src", exclude=("tests",)),
+        package_dir={"": "src"},
+        package_data={
+            "sandwitcher": ["dictionary.txt.gz"]
+        }
+    )
---
+++ src/sandwitcher/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os.path
+import random
+import gzip
+
+import click
+
+
+@click.command()
+@click.option("-c", "--capitalize", is_flag=True, help="Capitalize the first letter of each word.")
+@click.option("-l", "--word-length", type=int, default=5, help="The length of words to use.")
+@click.option("-n", "--word-number", type=int, default=2, help="The number of words to generate.")
+@click.option("-d", "--word-delimiter", type=str, default=" ", help="The word delimiter.")
+@click.option("-b", "--beginning", type=str, default="", help="The start of the password.")
+@click.option("-f", "--first-interstitial", type=str, default="", help="The first insterstitial string.")
+@click.option("-s", "--second-interstitial", type=str, default="", help="The second insterstitial string.")
+@click.option("-e", "--end", type=str, default="", help="The end of the password.")
+def main(capitalize, word_length, word_number, word_delimiter, beginning, first_interstitial, second_interstitial, end):
+    """
+    Generate a sandwich password from random words.
+    """
+    project_dir = os.path.dirname(os.path.realpath(__file__))
+    dict_file = os.path.join(project_dir, "dictionary.txt.gz")
+    with gzip.open(dict_file, "rt") as f:
+        word_selection = [word.strip() for word in f.readlines() if len(word.strip()) == word_length]
+
+    if len(word_selection) > 0:
+        final_words = list()
+        for n in range(word_number):
+            if capitalize:
+                final_words.append(random.choice(word_selection).capitalize())
+            else:
+                final_words.append(random.choice(word_selection))
+
+        print("{}{}{}{}{}".format(beginning, first_interstitial, word_delimiter.join(final_words), second_interstitial, end))
+    else:
+        print("No words of that length available.")
+
+
+if __name__ == "__main__":
+    main()
+
